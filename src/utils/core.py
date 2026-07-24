42b7e29bf73efee6a8ec7cc2b328782ec521947e
Anderson Paulino <andspaulino@gmail.com>
2026-07-24 13:30:44 -0300

    Add Linux screenshot capture foundation

---
+++ src/utils/core.py
@@ -0,0 +1,79 @@
+import cv2
+from mss import mss
+from farmhash import FarmHash64
+import numpy as np
+from typing import Callable, Union
+from src.shared.typings import BBox, GrayImage
+
+
+camera = None
+latestScreenshot = None
+
+
+def getCamera():
+    global camera
+    if camera is None:
+        camera = mss()
+    return camera
+
+
+# TODO: add unit tests
+def cacheObjectPosition(func: Callable) -> Callable:
+    lastX = None
+    lastY = None
+    lastW = None
+    lastH = None
+    lastImgHash = None
+
+    def inner(screenshot):
+        nonlocal lastX, lastY, lastW, lastH, lastImgHash
+        if lastX != None and lastY != None and lastW != None and lastH != None:
+            if hashit(screenshot[lastY:lastY + lastH, lastX:lastX + lastW]) == lastImgHash:
+                return (lastX, lastY, lastW, lastH)
+        res = func(screenshot)
+        if res is None:
+            return None
+        lastX = res[0]
+        lastY = res[1]
+        lastW = res[2]
+        lastH = res[3]
+        lastImgHash = hashit(
+            screenshot[lastY:lastY + lastH, lastX:lastX + lastW])
+        return res
+    return inner
+
+
+# TODO: add unit tests
+def hashit(arr: np.ndarray) -> int:
+    return FarmHash64(np.ascontiguousarray(arr))
+
+
+# TODO: add unit tests
+def locate(compareImage: GrayImage, img: GrayImage, confidence: float = 0.85) -> Union[BBox, None]:
+    match = cv2.matchTemplate(compareImage, img, cv2.TM_CCOEFF_NORMED)
+    res = cv2.minMaxLoc(match)
+    if res[1] <= confidence:
+        return None
+    return res[3][0], res[3][1], len(img[0]), len(img)
+
+
+# TODO: add unit tests
+def locateMultiple(compareImg: GrayImage, img: GrayImage, confidence: float = 0.85) -> Union[BBox, None]:
+    match = cv2.matchTemplate(compareImg, img, cv2.TM_CCOEFF_NORMED)
+    loc = np.where(match >= confidence)
+    resultList = []
+    for pt in zip(*loc[::-1]):
+        resultList.append((pt[0], pt[1], len(compareImg[0]), len(compareImg)))
+    return resultList
+
+
+# TODO: add unit tests
+def getScreenshot() -> GrayImage:
+    global latestScreenshot
+    screenCapture = getCamera()
+    screenshot = screenCapture.grab(screenCapture.monitors[1])
+    if screenshot is None:
+        return latestScreenshot
+    latestScreenshot = cv2.cvtColor(
+        np.asarray(screenshot), cv2.COLOR_BGRA2GRAY)
+    return latestScreenshot
---
+++ tests/unit/utils/test_core.py
@@ -0,0 +1,75 @@
+import cv2
+import numpy as np
+
+from src.utils import core
+
+
+class FakeCamera:
+    def __init__(self, screenshots):
+        self.monitors = [
+            {"left": 0, "top": 0, "width": 2, "height": 2},
+            {"left": 0, "top": 0, "width": 2, "height": 2},
+        ]
+        self.screenshots = iter(screenshots)
+        self.grabbedMonitors = []
+
+    def grab(self, monitor):
+        self.grabbedMonitors.append(monitor)
+        return next(self.screenshots)
+
+
+def setup_function():
+    core.camera = None
+    core.latestScreenshot = None
+
+
+def test_getScreenshot_converts_primary_monitor_bgra_frame_to_grayscale():
+    screenshot = np.array(
+        [
+            [[0, 0, 255, 255], [0, 255, 0, 255]],
+            [[255, 0, 0, 255], [255, 255, 255, 255]],
+        ],
+        dtype=np.uint8,
+    )
+    camera = FakeCamera([screenshot])
+    core.camera = camera
+
+    result = core.getScreenshot()
+
+    expected = cv2.cvtColor(screenshot, cv2.COLOR_BGRA2GRAY)
+    np.testing.assert_array_equal(result, expected)
+    assert result.shape == (2, 2)
+    assert result.dtype == np.uint8
+    assert camera.grabbedMonitors == [camera.monitors[1]]
+
+
+def test_getScreenshot_returns_latest_valid_frame_when_grab_returns_none():
+    screenshot = np.array([[[10, 20, 30, 255]]], dtype=np.uint8)
+    camera = FakeCamera([screenshot, None])
+    core.camera = camera
+
+    firstResult = core.getScreenshot()
+    secondResult = core.getScreenshot()
+
+    assert secondResult is firstResult
+
+
+def test_getScreenshot_returns_none_without_a_previous_valid_frame():
+    core.camera = FakeCamera([None])
+
+    assert core.getScreenshot() is None
+
+
+def test_getCamera_reuses_the_same_capture_instance(monkeypatch):
+    createdCameras = []
+    fakeCamera = FakeCamera([])
+
+    def createCamera():
+        createdCameras.append(fakeCamera)
+        return fakeCamera
+
+    monkeypatch.setattr(core, "mss", createCamera)
+
+    assert core.getCamera() is fakeCamera
+    assert core.getCamera() is fakeCamera
+    assert createdCameras == [fakeCamera]
