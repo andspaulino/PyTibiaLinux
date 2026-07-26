023eb8928c9d6745d74aabe89b531df7c66f657c
Anderson Paulino <andspaulino@gmail.com>
2026-07-25 22:14:07 -0300

    Implement Game Window And Radar Analysis

---
+++ src/repositories/gameWindow/slot.py
@@ -0,0 +1,35 @@
+from src.shared.typings import BBox, Slot
+from src.utils.mouse import leftClick, moveTo, rightClick
+
+
+# TODO: add unit tests
+# TODO: add perf
+def getSlotPosition(slot: Slot, gameWindowPosition: BBox) -> Slot:
+    (gameWindowPositionX, gameWindowPositionY, gameWindowWidth, gameWindowHeight) = gameWindowPosition
+    (slotX, slotY) = slot
+    slotHeight = gameWindowHeight // 11
+    slotWidth = gameWindowWidth // 15
+    slotXCoordinate = gameWindowPositionX + (slotX * slotWidth)
+    slotYCoordinate = gameWindowPositionY + (slotY * slotHeight)
+    return (slotXCoordinate, slotYCoordinate)
+
+
+# TODO: add unit tests
+# TODO: add perf
+def moveToSlot(slot: Slot, gameWindowPosition: BBox):
+    slotPosition = getSlotPosition(slot, gameWindowPosition)
+    moveTo(slotPosition)
+
+
+# TODO: add unit tests
+# TODO: add perf
+def clickSlot(slot: Slot, gameWindowPosition: BBox):
+    moveToSlot(slot, gameWindowPosition)
+    leftClick()
+
+
+# TODO: add unit tests
+# TODO: add perf
+def rightClickSlot(slot: Slot, gameWindowPosition: BBox):
+    moveToSlot(slot, gameWindowPosition)
+    rightClick()
