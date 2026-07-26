3771ffcb585d01c679051669bfec59cfa86575c9
Anderson Paulino <andspaulino@gmail.com>
2026-07-26 00:09:32 -0300

    Use Shared Tibia Window Discovery Utility
    Signed-off-by: Anderson Paulino <andspaulino@gmail.com>

--- src/gameplay/core/middlewares/window.py
+++ src/gameplay/core/middlewares/window.py
@@ -1,20 +1,11 @@
-import pygetwindow as gw
-import re
-import win32gui
 from ...typings import Context
+from src.utils.window import get_tibia_windows
 
 
 # TODO: add unit tests
 def setTibiaWindowMiddleware(context: Context) -> Context:
     if context['window'] is None:
-        windowsList: list = []
-        win32gui.EnumWindows(
-            lambda hwnd, param: param.append(hwnd), windowsList)
-        windowsNames = list(
-            map(lambda hwnd: win32gui.GetWindowText(hwnd), windowsList))
-        regex = re.compile(r'Tibia - .*')
-        windowsFilter = list(
-            filter(lambda windowName: regex.match(windowName), windowsNames))
-        if len(windowsFilter) > 0:
-            context['window'] = gw.getWindowsWithTitle(windowsFilter[0])[0]
+        tibia_windows = get_tibia_windows()
+        if len(tibia_windows) > 0:
+            context['window'] = tibia_windows[0]
     return context
